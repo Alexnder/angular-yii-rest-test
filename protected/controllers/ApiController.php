dae853b8993700c660d9ce5c0119b2eb1be80a6f
Alx <a.sergienko@hadrout.com>
2016-02-05 22:12:49 +0300

    Full json api

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -7,11 +7,9 @@ class ApiController extends Controller
         return array();
     }
 
-    // Actions
     public function actionList()
     {
-        // Get the respective model instance
-        switch($_GET['model'])
+        switch ($_GET['model'])
         {
             case 'articles':
                 $models = Article::model()->findAll();
@@ -20,50 +18,45 @@ class ApiController extends Controller
                 $models = User::model()->findAll();
                 break;
             default:
-                // Model not implemented error
-                $this->_sendResponse(501, sprintf(
-                    'Error: Mode <b>list</b> is not implemented for model <b>%s</b>',
-                    $_GET['model']) );
-                Yii::app()->end();
+                Helper::renderJSONErorr('list is not implemented for model '.$_GET['model']);
         }
-        // Did we get some results?
-        if(empty($models)) {
-            // No
-            $this->_sendResponse(200,
-                    sprintf('No items where found for model <b>%s</b>', $_GET['model']) );
-        } else {
-            // Prepare response
-            $rows = array();
-            foreach($models as $model)
-                $rows[] = $model->attributes;
-            // Send the response
-            $this->_sendResponse(200, CJSON::encode($rows));
+
+        if (empty($models))
+        {
+            Helper::renderJSONErorr('No items where found for model '.$_GET['model']);
         }
+
+        // Send results
+        $rows = array();
+        foreach($models as $model)
+        {
+            $rows[] = $model->attributes;
+        }
+        Helper::renderJSON($rows);
     }
 
     public function actionView()
     {
-        // Check if id was submitted via GET
-        if(!isset($_GET['id']))
-            $this->_sendResponse(500, 'Error: Parameter <b>id</b> is missing' );
-
-        switch($_GET['model'])
+        if (!isset($_GET['id']))
         {
-            // Find respective model
+            Helper::renderJSONErorr('Error: Parameter id is missing');
+        }
+
+        switch ($_GET['model'])
+        {
             case 'articles':
                 $model = Article::model()->findByPk($_GET['id']);
                 break;
             default:
-                $this->_sendResponse(501, sprintf(
-                    'Mode <b>view</b> is not implemented for model <b>%s</b>',
-                    $_GET['model']) );
-                Yii::app()->end();
+                Helper::renderJSONErorr('get is not implemented for model ' . $_GET['model']);
         }
-        // Did we find the requested model? If not, raise an error
-        if(is_null($model))
-            $this->_sendResponse(404, 'No Item found with id '.$_GET['id']);
-        else
-            $this->_sendResponse(200, CJSON::encode($model));
+
+        if (is_null($model))
+        {
+            Helper::renderJSONErorr('No Item found with id '.$_GET['id']);
+        }
+
+        Helper::renderJSON($model);
     }
 
     public function actionCreate()
@@ -72,28 +65,21 @@ class ApiController extends Controller
 
         switch($_GET['model'])
         {
-            // Get an instance of the respective model
             case 'articles':
                 $model = new Article;
                 break;
             default:
-                $this->_sendResponse(501,
-                    sprintf('Mode <b>create</b> is not implemented for model <b>%s</b>',
-                    $_GET['model']) );
-                    Yii::app()->end();
+                Helper::renderJSONErorr('create is not implemented for model '.$_GET['model']);
         }
 
         foreach ($_POST as $var => $value) {
-            // Does the model have this attribute? If not raise an error
             if ($model->hasAttribute($var))
             {
                 $model->$var = $value;
             }
             else
             {
-                $this->_sendResponse(500,
-                    sprintf('Parameter <b>%s</b> is not allowed for model <b>%s</b>', $var,
-                    $_GET['model']) );
+                Helper::renderJSONErorr("Parameter $var is not allowed for model ".$_GET['model']);
             }
         }
 
@@ -103,180 +89,105 @@ class ApiController extends Controller
         }
 
         // Try to save the model
-        if($model->save())
-            $this->_sendResponse(200, CJSON::encode($model));
-        else {
-            // Errors occurred
-            $msg = "<h1>Error</h1>";
-            $msg .= sprintf("Couldn't create model <b>%s</b>", $_GET['model']);
-            $msg .= "<ul>";
-            foreach($model->errors as $attribute=>$attr_errors) {
-                $msg .= "<li>Attribute: $attribute</li>";
-                $msg .= "<ul>";
-                foreach($attr_errors as $attr_error)
-                    $msg .= "<li>$attr_error</li>";
-                $msg .= "</ul>";
-            }
-            $msg .= "</ul>";
-            $this->_sendResponse(500, $msg );
+        if ($model->save())
+        {
+            Helper::renderJSON($model);
         }
+
+        $msg = sprintf("Couldn't create model %s\n", $_GET['model']);
+
+        foreach ($model->errors as $attribute=>$attr_errors)
+        {
+            $msg .= "Attribute: $attribute\n";
+            foreach ($attr_errors as $attr_error)
+            {
+                $msg .= "- $attr_error\n";
+            }
+        }
+        Helper::renderJSONErorr($msg);
     }
 
     public function actionUpdate()
     {
-        // Parse the PUT parameters. This didn't work: parse_str(file_get_contents('php://input'), $put_vars);
-        $json = file_get_contents('php://input'); //$GLOBALS['HTTP_RAW_POST_DATA'] is not preferred: http://www.php.net/manual/en/ini.core.php#ini.always-populate-raw-post-data
-        $put_vars = CJSON::decode($json,true);  //true means use associative array
+        $author = $this->_checkAuth();
 
-        switch($_GET['model'])
+        $json = file_get_contents('php://input');
+        $put_vars = CJSON::decode($json, true);
+
+        switch ($_GET['model'])
         {
-            // Find respective model
             case 'articles':
                 $model = Article::model()->findByPk($_GET['id']);
                 break;
             default:
-                $this->_sendResponse(501,
-                    sprintf( 'Error: Mode <b>update</b> is not implemented for model <b>%s</b>',
-                    $_GET['model']) );
-                Yii::app()->end();
+                Helper::renderJSONErorr('update is not implemented for model '.$_GET['model']);
         }
-        // Did we find the requested model? If not, raise an error
-        if($model === null)
-            $this->_sendResponse(400,
-                    sprintf("Error: Didn't find any model <b>%s</b> with ID <b>%s</b>.",
-                    $_GET['model'], $_GET['id']) );
+
+        if (is_null($model))
+        {
+            Helper::renderJSONErorr('Error: Didn\'t find any model '.$_GET['model'].' with ID '.$_GET['id']);
+        }
 
         // Try to assign PUT parameters to attributes
         foreach($put_vars as $var=>$value) {
             // Does model have this attribute? If not, raise an error
-            if($model->hasAttribute($var))
+            if ($model->hasAttribute($var))
+            {
                 $model->$var = $value;
-            else {
-                $this->_sendResponse(500,
-                    sprintf('Parameter <b>%s</b> is not allowed for model <b>%s</b>',
-                    $var, $_GET['model']) );
+            }
+            else
+            {
+                Helper::renderJSONErorr("Parameter $var is not allowed for model ".$_GET['model']);
             }
         }
-        // Try to save the model
-        if($model->save())
-            $this->_sendResponse(200, CJSON::encode($model));
-        else
-            // prepare the error $msg
-            // see actionCreate
-            // ...
-            $this->_sendResponse(500, $msg );
+
+        // TODO: check author
+
+        if ($model->save())
+        {
+            Helper::renderJSON($model);
+        }
+
+        $msg = sprintf("Couldn't update model %s\n", $_GET['model']);
+
+        foreach ($model->errors as $attribute=>$attr_errors)
+        {
+            $msg .= "Attribute: $attribute\n";
+            foreach ($attr_errors as $attr_error)
+            {
+                $msg .= "- $attr_error\n";
+            }
+        }
+        Helper::renderJSONErorr($msg);
     }
 
     public function actionDelete()
     {
-        switch($_GET['model'])
+        $author = $this->_checkAuth();
+
+        switch ($_GET['model'])
         {
-            // Load the respective model
             case 'articles':
                 $model = Article::model()->findByPk($_GET['id']);
                 break;
             default:
-                $this->_sendResponse(501,
-                    sprintf('Error: Mode <b>delete</b> is not implemented for model <b>%s</b>',
-                    $_GET['model']) );
-                Yii::app()->end();
+                Helper::renderJSONErorr('delete is not implemented for model '.$_GET['model']);
         }
-        // Was a model found? If not, raise an error
-        if($model === null)
-            $this->_sendResponse(400,
-                    sprintf("Error: Didn't find any model <b>%s</b> with ID <b>%s</b>.",
-                    $_GET['model'], $_GET['id']) );
+
+        if (is_null($model))
+        {
+            Helper::renderJSONErorr('Didn\'t find any model '.$_GET['model'].' with ID '.$_GET['id']);
+        }
+
+        // TODO: check author
 
         // Delete the model
-        $num = $model->delete();
-        if($num>0)
-            $this->_sendResponse(200, $num);    //this is the only way to work with backbone
-        else
-            $this->_sendResponse(500,
-                    sprintf("Error: Couldn't delete model <b>%s</b> with ID <b>%s</b>.",
-                    $_GET['model'], $_GET['id']) );
-    }
-
-    private function _sendResponse($status = 200, $body = '', $content_type = 'text/html')
-    {
-        // set the status
-        $status_header = 'HTTP/1.1 ' . $status . ' ' . $this->_getStatusCodeMessage($status);
-        header($status_header);
-        // and the content type
-        header('Content-type: ' . $content_type);
-
-        // pages with body are easy
-        if($body != '')
+        if ($model->delete())
         {
-            // send the body
-            echo $body;
+            Helper::renderJSON(true);
         }
-        // we need to create the body if none is passed
-        else
-        {
-            // create some body messages
-            $message = '';
 
-            // this is purely optional, but makes the pages a little nicer to read
-            // for your users.  Since you won't likely send a lot of different status codes,
-            // this also shouldn't be too ponderous to maintain
-            switch($status)
-            {
-                case 401:
-                    $message = 'You must be authorized to view this page.';
-                    break;
-                case 404:
-                    $message = 'The requested URL ' . $_SERVER['REQUEST_URI'] . ' was not found.';
-                    break;
-                case 500:
-                    $message = 'The server encountered an error processing your request.';
-                    break;
-                case 501:
-                    $message = 'The requested method is not implemented.';
-                    break;
-            }
-
-            // servers don't always have a signature turned on
-            // (this is an apache directive "ServerSignature On")
-            $signature = ($_SERVER['SERVER_SIGNATURE'] == '') ? $_SERVER['SERVER_SOFTWARE'] . ' Server at ' . $_SERVER['SERVER_NAME'] . ' Port ' . $_SERVER['SERVER_PORT'] : $_SERVER['SERVER_SIGNATURE'];
-
-            // this should be templated in a real-world solution
-            $body = '
-    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-    <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-        <title>' . $status . ' ' . $this->_getStatusCodeMessage($status) . '</title>
-    </head>
-    <body>
-        <h1>' . $this->_getStatusCodeMessage($status) . '</h1>
-        <p>' . $message . '</p>
-        <hr />
-        <address>' . $signature . '</address>
-    </body>
-    </html>';
-
-            echo $body;
-        }
-        Yii::app()->end();
-    }
-
-    private function _getStatusCodeMessage($status)
-    {
-        // these could be stored in a .ini file and loaded
-        // via parse_ini_file()... however, this will suffice
-        // for an example
-        $codes = Array(
-            200 => 'OK',
-            400 => 'Bad Request',
-            401 => 'Unauthorized',
-            402 => 'Payment Required',
-            403 => 'Forbidden',
-            404 => 'Not Found',
-            500 => 'Internal Server Error',
-            501 => 'Not Implemented',
-        );
-        return (isset($codes[$status])) ? $codes[$status] : '';
+        Helper::renderJSONErorr('Couldn\'t delete model '.$_GET['model'].' with ID '.$_GET['id']);
     }
 
     private function _checkAuth()
